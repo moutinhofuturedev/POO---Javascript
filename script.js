9684dcf2f27d513118083d3674d7a71f716a92a1
Paulo Moutinho <paulo_vicali@icloud.com>
2022-08-09 11:07:17 -0300

    practicing OOP concepts with functions

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ class Poligono {
 const soma = new Poligono(50, 60)
 
 console.log(soma.area)
+
+// exemplo de encapsulamento com function => (praticando o conceito dentro do POO)
+function Password(mypassword, yourpassword) {
+    this.mypassword = mypassword
+    this.yourpassword = yourpassword
+
+    let secretPassword = 12345
+
+    this.getPassword = () => {
+        return secretPassword
+    }
+
+    this.twoPasswords = () => {
+        return this.mypassword + ' e ' + this.yourpassword
+    }
+}
+
+const seePassword = new Password('Lau&Duda2020', 1007)
+console.log(seePassword.getPassword(), seePassword.twoPasswords())
